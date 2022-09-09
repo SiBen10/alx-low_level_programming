8b841a5f033ab0c726661dd1981d38705de099a4
SiBen10 <benjaminkn805@gmail.com>
2022-09-09 14:44:37 -0700

    Tebaphla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- *
- * Description: Prints all the numbers of base 16 in
- * lowercase, followed by a new line.
- *
- * Return: Always 0(Success)
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char hex_num, hex_alp;
+	char ch;
 
-	for (hex_num = 48; hex_num < 58; hex_num++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
-		putchar(hex_num);
+		putchar(ch);
 	}
-	for (hex_alp = 97; hex_alp <= 102; hex_alp++)
-	{
-		putchar(hex_alp);
-	}
-
 	putchar('\n');
-
 	return (0);
 }
