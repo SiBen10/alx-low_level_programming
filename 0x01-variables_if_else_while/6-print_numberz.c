77844a2021ef6e28391a5003c63e751d5781c4b3
SiBen10 <benjaminkn805@gmail.com>
2022-09-09 14:25:44 -0700

    PRINT NUMBERS LESS THAN 58

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints all single digit numbers of
+ * base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
