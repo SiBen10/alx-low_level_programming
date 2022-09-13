79e55f68a84672e517f17bc0fadef5f6e34274f9
SiBen10 <benjaminkn805@gmail.com>
2022-09-13 14:28:02 -0700

    PRINT ALPHABETS LOWERCASE

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @f: the character to check
+ * Return: 1 if f is lowercase, 0 otherwise
+ */
+int _islower(int f)
+{
+	return (f >= 'a' && f <= 'z');
+}
